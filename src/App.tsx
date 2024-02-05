0473531a9662e00adec21e5a984067ec46349bf2
Yuriy Ulanov <yuriyul@MacBook-Pro-Yuriy.local>
2024-02-05 22:15:24 +0300

    Lesson 6 / Routing, lazy load
    1. add react-router-dom
    2. add routes
    3. Split code to chunks
    4. Suspense, lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import { Counter } from './components/Counter';
+import { Link, Route, Routes } from 'react-router-dom';
+// import { AboutPage } from './pages/AboutPage/AboutPage';
+import { AboutPageAsync } from './pages/AboutPage/AboutPageAsync';
+import { MainPage } from './pages/MainPage/MainPage';
+
 import './index.scss';
+import { Suspense } from 'react';
 
 export const App = () => {
     return (
-        <div className='app'>
-            test text
-            <Counter />
+        <div className="app">
+            <Link to={'/'}>Main page</Link>
+            <Link to={'/about'}>About page</Link>
+            <Suspense fallback="loading...">
+                <Routes>
+                    <Route path={'/'} element={<MainPage />} />
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 };
