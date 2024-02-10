f2e1b9ba2ac48baffc79c194dd2a434afcf0950b
Yuriy Ulanov <yuriyul@MacBook-Pro-Yuriy.local>
2024-02-10 20:27:34 +0300

    Lesson 10 / architecture 'feature-sliced'
    1. change folders structure
    2. adjust absolute pahts
    3. distributed the files according to the methodology

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import { Link, Route, Routes } from 'react-router-dom';
-import { AboutPageAsync } from './pages/AboutPage/AboutPageAsync';
-import { MainPageAsync } from './pages/MainPage/MainPageAsync';
+import { AboutPage } from '../pages/AboutPage';
+import { MainPage } from './../pages/MainPage';
 
 import { Suspense } from 'react';
 import './styles/index.scss';
-import { useTheme } from './theme/useTheme';
-import { classNames } from './helpers/classNames/classNames';
+import { useTheme } from './providers/ThemeProvider';
+import { classNames } from './../shared/lib/classNames/classNames';
 
 export const App = () => {
     const { theme, toggleTheme } = useTheme();
@@ -16,8 +16,8 @@ export const App = () => {
             <Link to="/about">About page</Link>
             <Suspense fallback={<div>loading...</div>}>
                 <Routes>
-                    <Route path="/" element={<MainPageAsync />} />
-                    <Route path="/about" element={<AboutPageAsync />} />
+                    <Route path="/" element={<MainPage />} />
+                    <Route path="/about" element={<AboutPage />} />
                 </Routes>
             </Suspense>
             <button onClick={toggleTheme}>Change theme</button>
