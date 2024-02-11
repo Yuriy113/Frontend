a34819c2c0bc48429e4d13303e91380462dae122
Yuriy Ulanov <yuriyul@MacBook-Pro-Yuriy.local>
2024-02-11 13:41:10 +0300

    Lesson 12 / Navbar
    1. add navbar widget
    2. add uiKit component (AppLink)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,17 @@
-import { Link } from 'react-router-dom';
 import { useTheme } from './providers/ThemeProvider';
 import { classNames } from './../shared/lib/classNames/classNames';
 import { AppRouter } from './providers/router';
 
 import './styles/index.scss';
+import { Navbar } from 'widgets/Navbar';
 
 export const App = () => {
     const { theme, toggleTheme } = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Link to="/">Main page</Link>
-            <Link to="/about">About page</Link>
+            <Navbar />
             <AppRouter />
-
             <div>
                 <button onClick={toggleTheme}>Change theme</button>
             </div>
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,19 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+
+import cls from './navbar.module.scss';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+
+interface NavbarProps {
+    className?: string;
+}
+
+export const Navbar = ({ className }: NavbarProps) => {
+    return (
+        <div className={classNames(cls.navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink theme={AppLinkTheme.SECONDARY} to="/">Main page</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to="/about">About page</AppLink>
+            </div>
+        </div>
+    );
+};
